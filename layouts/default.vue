346353d1fde592ceae0083fb21fb15df9c7236d2
Daniele Pezzatini <daniele@bestiario.org>
2019-10-29 18:34:14 +0100

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,35 @@
+<template>  
+  <div>
+    <Header />
+    <div class="uk-section uk-section-default">
+      <div class="uk-container uk-container-large">
+        <nuxt />
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+// Import your new Header component
+import Header from '~/components/Header.vue'
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>  
+  <div>
+    <img src="https://media.giphy.com/media/zBL9j9oiR3VM4/giphy.gif" class="uk-position-center" alt="">
+  </div>
+</template> 
+
+<script>
+export default {
+  components: {
+  }
+}
+</script>
+
+<style>
+</style>
